2d0f11db757d57daa11983baaffd73d15a98c5c0
Lin Yanzhou <986914265@qq.com>
2018-01-29 19:27:39 +0800

    tinyos

--- 一步一步写嵌入式操作系统/TinyOS/keil/Objects/tbitmap.d
+++
@@ -1,4 +0,0 @@
-.\objects\tbitmap.o: ..\src\tBitmap.c
-.\objects\tbitmap.o: ..\src\tLib.h
-.\objects\tbitmap.o: ..\src\global.h
-.\objects\tbitmap.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- 一步一步写嵌入式操作系统/TinyOS/keil/Objects/tlist.d
+++
@@ -1,4 +0,0 @@
-.\objects\tlist.o: ..\src\tList.c
-.\objects\tlist.o: ..\src\tLib.h
-.\objects\tlist.o: ..\src\global.h
-.\objects\tlist.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
